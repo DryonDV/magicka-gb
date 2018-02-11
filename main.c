6dd52019a14d93703ecf3b71c7f1e30173fce5a0
Dryon <36274971+DryonDV@users.noreply.github.com>
2018-02-11 13:40:45 +0100

    elements menu added

--- main.c
+++ main.c
@@ -27,8 +27,10 @@ void walkanim();
 void lifeposition();
 void assignsprite();
 void assignpalette();
+void assigntilespalette();
 void isitloaded();
 void elementsmenu();
+void assigntiles();
 
 UINT8 player_pos[2];
 UINT8 player_tiles_left[2];
@@ -45,13 +47,24 @@ UINT8 lifebar_pos[2];
 UINT8 movelistorderstart;
 UINT8 movelistordercurrent;
 UINT8 tiletoassign;
-
+UINT8 movelistordernext;
 UINT8 spritenb;
 UINT8 spriteorder;
 UINT8 paletteorder;
 UINT8 magicka_player_loaded;
+UINT8 elementsmenu_loaded;
 UINT8 player_move_allowed;
-
+UINT8 quitelementsmenu;
+UINT8 recall_player_pos[2];
+UINT8 elementsmenu_palette_loaded;
+UINT8 magicka_player_palette_loaded;
+UINT8 elements_tilesstart;
+UINT8 magicka_player_tilesstart;
+UINT8 interface_tilesstart;
+UINT8 elementsmenu_tiles_assigned;
+UINT8 magicka_player_tiles_assigned;
+UINT8 magicka_player_tiles_loaded;
+UINT8 elementsmenu_tiles_loaded;
 
 UWORD testworld_palette_bw[] = {
 	//p0
@@ -110,8 +123,22 @@ UWORD magicka_player_palette[] = {
 };
 
 UWORD elements_palette[] = {
-	//fire
-	 elementsCGBPal0c0,elementsCGBPal0c1,elementsCGBPal0c2,elementsCGBPal0c3
+	//scroll
+	 elementsCGBPal0c0,elementsCGBPal0c1,elementsCGBPal0c2,elementsCGBPal0c3,
+	 //water
+	 elementsCGBPal1c0,elementsCGBPal1c1,elementsCGBPal1c2,elementsCGBPal1c3,
+	 //life
+	 elementsCGBPal2c0,elementsCGBPal2c1,elementsCGBPal2c2,elementsCGBPal2c3,
+	 //ice
+	 elementsCGBPal3c0,elementsCGBPal3c1,elementsCGBPal3c2,elementsCGBPal3c3,
+	 //thunder
+	 elementsCGBPal4c0,elementsCGBPal4c1,elementsCGBPal4c2,elementsCGBPal4c3,
+	 //mana
+	 elementsCGBPal5c0,elementsCGBPal5c1,elementsCGBPal5c2,elementsCGBPal5c3,
+	 //earth
+	 elementsCGBPal6c0,elementsCGBPal6c1,elementsCGBPal6c2,elementsCGBPal6c3,
+	 //fire
+	 elementsCGBPal7c0,elementsCGBPal7c1,elementsCGBPal7c2,elementsCGBPal7c3,
 };
 
 	 
@@ -137,23 +164,34 @@ void isitloaded() {
 	if (spritenb == 0) {
 		magicka_player_loaded = 1;
 	}
+	else if (spritenb == 2) {
+		elementsmenu_loaded = 1;
+	}
 }
 
 
-void assignpalette() {
+void assigntilespalette() {
 	if (spritenb == 0) {
 		
 		set_sprite_prop(0,0);
 		set_sprite_prop(1,2);
 		set_sprite_prop(2,1);
 		set_sprite_prop(3,1);
+		set_sprite_prop(4,0);
 		set_sprite_prop(5,2);
 		set_sprite_prop(6,1);
+		set_sprite_prop(7,0);
+		set_sprite_prop(8,0);
 		set_sprite_prop(9,2);
 		set_sprite_prop(10,1);
+		set_sprite_prop(11,0);
+		set_sprite_prop(12,0);
 		set_sprite_prop(13,2);
 		set_sprite_prop(14,1);
 		set_sprite_prop(15,1);
+		set_sprite_prop(16,0);
+		set_sprite_prop(17,0);
+		
 	}
 	else if (spritenb == 1) {
 		set_sprite_prop(18,3);
@@ -171,34 +209,124 @@ void assignpalette() {
 		set_sprite_prop(30,3);
 	}
 	else if (spritenb == 2) {
-		set_sprite_prop(31,4);
-		set_sprite_prop(32,4);
-		set_sprite_prop(33,4);
-		set_sprite_prop(34,4);
+		
+		set_sprite_prop(0,1);
+		set_sprite_prop(1,1);
+		set_sprite_prop(2,1);
+		set_sprite_prop(3,1);
+		set_sprite_prop(4,2);
+		set_sprite_prop(5,2);
+		set_sprite_prop(6,2);
+		set_sprite_prop(7,2);
+		set_sprite_prop(8,3);
+		set_sprite_prop(9,3);
+		set_sprite_prop(10,3);
+		set_sprite_prop(11,3);
+		set_sprite_prop(12,4);
+		set_sprite_prop(13,4);
+		set_sprite_prop(14,4);
+		set_sprite_prop(15,4);
+		set_sprite_prop(16,5);
+		set_sprite_prop(17,5);
+		set_sprite_prop(18,5);
+		set_sprite_prop(19,5);
+		set_sprite_prop(20,6);
+		set_sprite_prop(21,6);
+		set_sprite_prop(22,6);
+		set_sprite_prop(23,6);
+		set_sprite_prop(24,7);
+		set_sprite_prop(25,7);
+		set_sprite_prop(26,7);
+		set_sprite_prop(27,7);
+		set_sprite_prop(28,0);
+		set_sprite_prop(29,0);
+		set_sprite_prop(30,0);
+		set_sprite_prop(31,0);
+		
 		}
 		
 }
 		
-void assignsprite() {
-		
-	set_sprite_data(spriteorder, tilescount[spritenb], spritereference[spritenb]);
-	
-	spriteorder = spriteorder+tilescount[spritenb];
 
-	//tilesheet -> VRAM 
-	//VRAM -> sprite list
+void assignpalette() {
+	if (spritenb == 0){
+		paletteorder = 0;
+	}
+	else if (spritenb == 2){
+		paletteorder = 0;
+	}
 	
-	while (tiletoassign < movelistorderstart+tilescount[spritenb]) {
+	if (paletteorder+palettecount[spritenb] > 7) {
+		paletteorder = 0;
+	}
+	
+	
+	set_sprite_palette(paletteorder,palettecount[spritenb],palettereference[spritenb]);
+	
+	paletteorder = paletteorder+palettecount[spritenb];	
+}
+
+void assignsprite() {
+	if (spritenb==0 && magicka_player_tiles_loaded==0) {
+		set_sprite_data(spriteorder, tilescount[spritenb], spritereference[spritenb]);
+		spriteorder = spriteorder+tilescount[spritenb];
+		magicka_player_tiles_loaded = 1;
+		assigntiles(spritenb=0);
+	}
+	
+	else if (spritenb==2 && elementsmenu_tiles_loaded==0){
+		
+		set_sprite_data(spriteorder, tilescount[spritenb], spritereference[spritenb]);
+		spriteorder = spriteorder+tilescount[spritenb];
+		elementsmenu_tiles_loaded=1;
+		assigntiles(spritenb=2);
+	}
+	
+	else if (spritenb != 2 && spritenb !=0){
+		
+		set_sprite_data(spriteorder, tilescount[spritenb], spritereference[spritenb]);
+		spriteorder = spriteorder+tilescount[spritenb];
+		assigntiles(spritenb=spritenb);
+	}
+}
+	//tilesheet -> VRAM 
+	//VRAM -> sprite list(OAM)
+	
+void assigntiles() {
+	if(spritenb==0){
+			movelistordercurrent=0;
+			tiletoassign=0;
+	}
+	if (spritenb==2){
+			movelistordercurrent=0;
+			tiletoassign=18;
+			movelistordernext = 18;
+			
+	}
+	
+	while (tiletoassign < (movelistordernext+tilescount[spritenb])) {
+
 		set_sprite_tile(movelistordercurrent,tiletoassign);
 		movelistordercurrent++;
 		tiletoassign++;
 	}
-	movelistorderstart = movelistordercurrent;
+	movelistorderstart = movelistordercurrent-tilescount[spritenb];
+	movelistordernext = movelistordercurrent;
 	
-	set_sprite_palette(paletteorder,palettecount[spritenb],palettereference[spritenb]);
-	paletteorder = paletteorder+palettecount[spritenb];
+	if (spritenb == 2) {
+		elements_tilesstart = movelistorderstart;
+		elementsmenu_tiles_assigned = 1;
+		magicka_player_tiles_assigned = 0;
+		
+	}
+	 if (spritenb == 0) {
+		elementsmenu_tiles_assigned = 0;
+		magicka_player_tiles_assigned = 1;
+	}
+		
 	
 	assignpalette();
+	assigntilespalette();
 	isitloaded();
 	
 }
@@ -249,8 +377,7 @@ void init() {
 	
 	
 	assignsprite(spritenb=0);
-	assignsprite(spritenb=1);
-	assignsprite(spritenb=2);
+	// assignsprite(spritenb=1);
 	// lifeposition();
 }
 void updateSwitches() {
@@ -260,6 +387,125 @@ void updateSwitches() {
 }
 
 void elementsmenu() {
+	
+	quitelementsmenu = 0;
+	
+	recall_player_pos[0] = player_pos[0];
+	recall_player_pos[1] = player_pos[1];
+	move_sprite(player_tiles_left[0],200,200);
+	move_sprite(player_tiles_left[1],200,200);
+	move_sprite(player_tiles_right[0],200,200);
+	move_sprite(player_tiles_right[1],200,200);
+	
+	
+	if(elementsmenu_tiles_loaded == 0) {
+		movelistordercurrent = 0;
+		assignsprite(spritenb=2);
+		elementsmenu_palette_loaded = 1;
+		
+	}
+	
+	else if (elementsmenu_tiles_assigned == 0) {
+		
+		// elements_tilesstart=0;
+		movelistordercurrent = 0;
+		movelistordernext=0;
+		tiletoassign = 0;
+		assigntiles(spritenb=2);
+		// DISPLAY_OFF;
+	}
+	
+	else if(elementsmenu_tiles_assigned ==1){
+		move_sprite(1,30,30);
+	}
+	
+	else if (elementsmenu_palette_loaded == 0) {
+		
+		assignpalette(spritenb=2);
+		elementsmenu_palette_loaded = 1;
+	}
+	
+	player_move_allowed = 0;
+	set_bkg_palette(0,5,&testworld_palette_bw[0]);
+	
+	//UP
+	move_sprite(0,80,18);
+	move_sprite(1,88,18);
+	move_sprite(2,80,26);
+	move_sprite(3,88,26);
+	//UPRIGHT
+	move_sprite(4,133,38);
+	move_sprite(5,141,38);
+	move_sprite(6,133,46);
+	move_sprite(7,141,46);
+	//RIGHT
+	move_sprite(8,150,80);
+	move_sprite(9,158,80);
+	move_sprite(10,150,88);
+	move_sprite(11,158,88);
+	//DOWNRIGHT
+	move_sprite(12,133,122);
+	move_sprite(13,141,122);
+	move_sprite(14,133,130);
+	move_sprite(15,141,130);
+	
+	//DOWN
+	// move_sprite(elements_tilesstart+,80,142);
+	// move_sprite(elements_tilesstart+,88,142);
+	// move_sprite(elements_tilesstart+,80,150);
+	// move_sprite(elements_tilesstart+,88,150);
+	
+	//DOWNLEFT
+	move_sprite(16,27,122);
+	move_sprite(17,35,122);
+	move_sprite(18,27,130);
+	move_sprite(19,35,130);
+	//LEFT
+	move_sprite(20,10,80);
+	move_sprite(21,18,80);
+	move_sprite(22,10,88);
+	move_sprite(23,18,88);
+	//UPLEFT
+	move_sprite(24,30,38);
+	move_sprite(25,38,38);
+	move_sprite(26,30,46);
+	move_sprite(27,38,46);
+	
+	//BOX
+	move_sprite(28,40,75);
+	move_sprite(29,40,83);
+	move_sprite(30,40,88);
+	
+	
+	
+	
+	while(quitelementsmenu == 0) {
+	checkInput();
+		if((joypad() & J_A)) {
+			int u = movelistorderstart;
+			int i=0;
+			
+			player_move_allowed = 1;
+			set_bkg_palette(0,5,&testworld_palette[0]);
+			spritenb=2;
+			while (i<tilescount[spritenb]) {
+				
+				move_sprite(u,200,200);
+				i++;
+				u++;
+				
+			}
+			quitelementsmenu = 1;
+			
+			paletteorder = 0;
+			assigntiles(spritenb=0);
+			assignpalette(spritenb=0);
+			movelistordernext=0;
+			elementsmenu_palette_loaded = 0;
+			player_pos[0] = recall_player_pos[0];
+			player_pos[1] = recall_player_pos[1];
+		}
+	}
 }
 
 void lifeposition() {
@@ -331,18 +577,14 @@ void walkanim() {
 void checkInput() {
 
     if ((joypad() & J_B) && (player_move_allowed == 1)) {
-		player_move_allowed = 0;
-		set_bkg_palette(0,5,&testworld_palette_bw[0]);
-		move_sprite(34,50,50);
+		elementsmenu();
 	}	
 	
 	
 	
 	
 	if((joypad() & J_A) ) {
-		player_move_allowed = 1;
-		set_bkg_palette(0,5,&testworld_palette[0]);
-		
+	
     }
 	
 	if ((magicka_player_loaded == 1) && (player_move_allowed == 1)) {
